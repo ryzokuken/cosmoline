2f159241fa8bff81fd150bca25dae3fb4eab3b42
Ujjwal Sharma <ryzokuken@disroot.org>
2021-03-21 00:05:13 +0530

    src: add basic options, config and secrets handling
    - Handles basic options
    - Reads from a config file
    - Locates secrets
    - Generates keypair if none exists

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,85 @@
+use clap::{load_yaml, App};
+use ed25519_dalek::{ExpandedSecretKey, Keypair, PublicKey};
+use rand::rngs::OsRng;
+use regex::Regex;
+
+use std::fs::File;
+use std::path::PathBuf;
+
+use std::io::prelude::*;
+
+struct ExpandedKeypair {
+    public: PublicKey,
+    secret: ExpandedSecretKey,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let options = load_yaml!("options.yaml");
+    let matches = App::from(options).get_matches();
+
+    let config_file = match matches.value_of("config") {
+        Some(path) => PathBuf::from(path),
+        None => dirs::config_dir()
+            .unwrap()
+            .join("cosmoline")
+            .join("config.toml"),
+    };
+    let mut config_file = File::open(config_file).unwrap();
+    let mut config = String::new();
+    config_file.read_to_string(&mut config).unwrap();
+    let config: toml::Value = toml::from_str(config.as_str()).unwrap();
+
+    let path = match config.as_table().unwrap().get("path") {
+        Some(path) => PathBuf::from(path.as_str().unwrap()),
+        None => dirs::home_dir().unwrap().join(".cosmoline"),
+    };
+    let secret_path = path.join("secret");
+    let keypair = if secret_path.exists() {
+        let mut secret_file = File::open(secret_path).unwrap();
+        let mut secret = String::new();
+        secret_file.read_to_string(&mut secret).unwrap();
+        let re = Regex::new(r"\s*#[^\n]*").unwrap();
+        let secret = re.replace_all(secret.as_str(), "");
+        let secret = match json::parse(&secret).unwrap() {
+            json::JsonValue::Object(obj) => obj,
+            _ => panic!("invalid secret file"),
+        };
+
+        if secret.get("curve").unwrap().as_str().unwrap() != "ed25519" {
+            panic!("wrong curve");
+        }
+
+        let pubkey = secret
+            .get("public")
+            .unwrap()
+            .as_str()
+            .unwrap()
+            .replace(".ed25519", "");
+        let pubkey = base64::decode(pubkey).unwrap();
+        let pubkey = PublicKey::from_bytes(pubkey.as_slice()).unwrap();
+
+        let privkey = secret
+            .get("private")
+            .unwrap()
+            .as_str()
+            .unwrap()
+            .replace(".ed25519", "");
+        let privkey = base64::decode(privkey).unwrap();
+        let privkey = ExpandedSecretKey::from_bytes(privkey.as_slice()).unwrap();
+
+        ExpandedKeypair {
+            public: pubkey,
+            secret: privkey,
+        }
+    } else {
+        let mut csprng = OsRng {};
+        let keypair = Keypair::generate(&mut csprng);
+        ExpandedKeypair {
+            public: keypair.public,
+            secret: ExpandedSecretKey::from(&keypair.secret),
+        }
+        // TODO: write this keypair to a fresh secret file
+    };
+    println!("{:?}", keypair.public.to_bytes());
+    println!("{:?}", keypair.secret.to_bytes());
 }
