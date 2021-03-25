433139cfb1c3d705f3b58cbd7a3638aa77d02f33
Ujjwal Sharma <ryzokuken@disroot.org>
2021-03-25 11:14:00 +0530

    src: allow users to specify custom path

--- src/main.rs
+++ src/main.rs
@@ -76,9 +76,9 @@ fn read_config(path: PathBuf) -> Config {
 
 fn main() {
     let options = load_yaml!("options.yaml");
-    let matches = App::from(options).get_matches();
+    let options = App::from(options).get_matches();
 
-    let config_file = match matches.value_of("config") {
+    let config_file = match options.value_of("config") {
         Some(path) => PathBuf::from(path),
         None => dirs::config_dir()
             .unwrap()
@@ -87,9 +87,12 @@ fn main() {
     };
     let config = read_config(config_file);
 
-    let path = match config.as_table().unwrap().get("path") {
-        Some(path) => PathBuf::from(path.as_str().unwrap()),
-        None => dirs::home_dir().unwrap().join(".cosmoline"),
+    let path = match options.value_of("path") {
+        Some(path) => PathBuf::from(path),
+        None => match config.get("path") {
+            Some(path) => PathBuf::from(path.as_str().unwrap()),
+            None => dirs::home_dir().unwrap().join(".cosmoline"),
+        }
     };
     let keypair = Keypair::read_or_generate(path.join("secret"));
     println!("{}", keypair.to_json().pretty(2));
