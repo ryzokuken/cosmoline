60cf753b7f2b1dc6effea69cc30b03f9478fefcb
mirsal <mirsal@mirsal.fr>
2021-03-30 08:45:33 +0000

    main: Fix the main function return type

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ fn parse_packet(packet: String) -> Host {
 }
 
 #[async_std::main]
-async fn main() -> std::io::Result<()> {
+async fn main() {
     let options = load_yaml!("options.yaml");
     let options = App::from(options).get_matches();
 
@@ -45,7 +45,7 @@ async fn main() -> std::io::Result<()> {
             .join("cosmoline")
             .join("config.toml")),
     };
-    let config = fs::read_to_string(config_file).await?;
+    let config = fs::read_to_string(config_file).await.unwrap();
     let config: Config = toml::from_str(config.as_str()).unwrap();
 
     let path = match options.value_of("path") {
@@ -58,14 +58,14 @@ async fn main() -> std::io::Result<()> {
     let keypair = Keypair::read_or_generate(path.join("secret")).await;
     println!("{}", keypair.to_json().pretty(2));
 
-    let socket = UdpSocket::bind("0.0.0.0:8008").await?;
+    let socket = UdpSocket::bind("0.0.0.0:8008").await.unwrap();
     let mut buf = [0u8; 1024];
 
     loop {
-        let (amt, peer) = socket.recv_from(&mut buf).await?;
+        let (amt, peer) = socket.recv_from(&mut buf).await.unwrap();
         let buf = &mut buf[..amt];
         let packet = String::from_utf8(buf.to_vec()).unwrap();
         println!("{:?}", (peer, parse_packet(packet)));
-    }
+    };
 
 }
