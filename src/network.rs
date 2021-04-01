48c2270f3a5c68bd454a2fb2ab5ccb0b727e9e17
mirsal <mirsal@mirsal.fr>
2021-04-01 11:08:45 +0000

    Move the peer discovery send/recv tasks to the network module

--- src/network.rs
+++ src/network.rs
@@ -1,4 +1,6 @@
-use async_std::net::IpAddr;
+use async_std::task;
+use async_std::net::{IpAddr, UdpSocket};
+use async_std::sync::Arc;
 use ed25519_dalek::PublicKey;
 
 use crate::keypair::SSBPublicKey;
@@ -56,5 +58,36 @@ impl Peer {
             port,
             pubkey,
         }
+    }
+}
+
+pub async fn peer_discovery_recv() {
+    let socket = UdpSocket::bind(":::8008").await.unwrap();
+    let mut buf = [0u8; 1024];
+
+    loop {
+        let (amt, peer) = socket.recv_from(&mut buf).await.unwrap();
+        let buf = &mut buf[..amt];
+        let packet = String::from_utf8(buf.to_vec()).unwrap();
+        println!(
+            "{} {}",
+            peer,
+            Peer::from_discovery_packet(&packet).to_discovery_packet()
+        );
+    }
+}
+
+pub async fn peer_discovery_send(pubkey: Arc<String>) {
+    let socket = UdpSocket::bind(":::0").await.unwrap();
+    let msg = format!("net:1.2.3.4:8023~shs:{}", &pubkey);
+    let buf = msg.as_bytes();
+
+    socket.set_broadcast(true).unwrap();
+
+    loop {
+        println!("Sending packet: {:?}", &msg);
+        socket.send_to(&buf, "255.255.255.255:8008").await.unwrap();
+        socket.send_to(&buf, "[FF02::1]:8008").await.unwrap();
+        task::sleep(std::time::Duration::from_secs(1)).await;
     }
 }
